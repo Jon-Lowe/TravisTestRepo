99532b3b63c0890f6e0cb89dcafa1e295ed691d1
Jonathan Lowe <100577290@student.swin.edu.au>
2019-05-04 08:59:08 +1000

    Sln commit

---
+++ TravisTestSln/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravisTestSln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
